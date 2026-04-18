0401ed9910f4c5058b5c27b5557432e69b4af392
noellegazelle6 <noellegazelle6@gmail.com>
2026-04-18 12:19:34 +0800

    update
    删除无用的

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.kail.location"
         minSdk = 27
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.4.4"
+        versionCode = 16
+        versionName = "1.4.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
